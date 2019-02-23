395af93bb49aec9710a74f1f28fda13792745ba7
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2019-02-23 17:17:55 +0000

    Initial commit

---
+++ cmd/update_wallpapers/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"cloud.google.com/go/firestore"
+
+	"google.golang.org/api/iterator"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	err := Bootstrap()
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Bootstrap error: "+err.Error())
+	}
+}
+
+func Bootstrap() error {
+	ctx := context.Background()
+
+	sa := option.WithCredentialsFile("serviceAccount.json")
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	resp, _ := http.Get("https://www.bing.com/HPImageArchive.aspx?format=js&n=10&mbl=1&mkt=en-ww")
+	defer resp.Body.Close()
+
+	var v map[string][]map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&v)
+	n := v["images"]
+
+	for _, v := range n {
+		name := strings.Replace(v["urlbase"].(string), "/az/hprichbg/rb/", "", 1)
+		name = strings.Replace(name, "/th?id=OHR.", "", 1)
+		x := strings.Split(name, "_")
+		name = x[0]
+		_, err = client.Collection("wallpapers").Doc(name).Set(ctx, map[string]interface{}{
+			"title": v["copyright"],
+			"name":  name,
+			"date":  v["startdate"],
+		})
+		if err != nil {
+			log.Fatalf("Failed adding: %v", err)
+		}
+	}
+
+	iter := client.Collection("wallpapers").OrderBy("date", firestore.Desc).Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Println(doc.Data())
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module api
+
+require (
+	cloud.google.com/go v0.36.0
+	firebase.google.com/go v3.6.0+incompatible
+	github.com/go-chi/chi v4.0.1+incompatible
+	github.com/go-sql-driver/mysql v1.4.0
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/julienschmidt/httprouter v1.2.0 // indirect
+	github.com/rs/zerolog v1.11.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20190220154721-9b3c75971fc9 // indirect
+	google.golang.org/api v0.1.0
+)

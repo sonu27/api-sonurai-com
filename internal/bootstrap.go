dbdc36a154b0c53389498f6e9b8251e652c09422
Amarjeet Rai <jatt27@gmail.com>
2020-01-12 15:29:08 +0000

    Add label annotations

--- internal/bootstrap.go
+++ internal/bootstrap.go
@@ -33,7 +33,11 @@ var (
 	cache           *bigcache.BigCache
 )
 
-type Image struct {
+type ListResponse struct {
+	Data []ImageBasic `json:"data"`
+}
+
+type ImageBasic struct {
 	ID        string `json:"id"`
 	Title     string `json:"title"`
 	Copyright string `json:"copyright"`
@@ -42,8 +46,21 @@ type Image struct {
 	Market    string `json:"market"`
 }
 
-type ListResponse struct {
-	Data []Image `json:"data"`
+type Image struct {
+	ID               string            `json:"id"`
+	Title            string            `json:"title"`
+	Copyright        string            `json:"copyright"`
+	Date             int               `json:"date"`
+	Filename         string            `json:"filename"`
+	Market           string            `json:"market"`
+	LabelAnnotations []LabelAnnotation `json:"labelAnnotations,omitempty"`
+}
+
+type LabelAnnotation struct {
+	Description string  `json:"description"`
+	MID         string  `json:"mid"`
+	Score       float64 `json:"score"`
+	Topicality  float64 `json:"topicality"`
 }
 
 func Bootstrap() error {
@@ -170,7 +187,7 @@ func listWallpapersHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		var wallpaper Image
+		var wallpaper ImageBasic
 		mapstructure.Decode(doc.Data(), &wallpaper)
 		res.Data = append(res.Data, wallpaper)
 	}

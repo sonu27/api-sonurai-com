0e7d5603b84f282c64b3038f5780e0217a4f055f
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2021-03-26 11:48:11 +0000

    Change format of tags

--- internal/model/model.go
+++ internal/model/model.go
@@ -14,18 +14,11 @@ type ImageBasic struct {
 }
 
 type Image struct {
-	ID               string            `json:"id"`
-	Title            string            `json:"title"`
-	Copyright        string            `json:"copyright"`
-	Date             int               `json:"date"`
-	Filename         string            `json:"filename"`
-	Market           string            `json:"market"`
-	LabelAnnotations []LabelAnnotation `json:"labelAnnotations,omitempty"`
-}
-
-type LabelAnnotation struct {
-	Description string  `json:"description"`
-	MID         string  `json:"mid"`
-	Score       float64 `json:"score"`
-	Topicality  float64 `json:"topicality"`
+	ID        string             `json:"id"`
+	Title     string             `json:"title"`
+	Copyright string             `json:"copyright"`
+	Date      int                `json:"date"`
+	Filename  string             `json:"filename"`
+	Market    string             `json:"market"`
+	Tags      map[string]float64 `json:"tags"`
 }

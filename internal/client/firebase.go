defa26d1b0e95c2e5e7bb8808fe37a148f3c6af9
Amarjeet Singh Rai <git@arai.dev>
2022-09-23 16:30:56 +0100

    feat: add updating of wallpapers from pubsub

--- internal/client/firebase.go
+++ internal/client/firebase.go
@@ -5,26 +5,16 @@ import (
 	"encoding/base64"
 	"os"
 
-	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
 )
 
-func FirestoreClient(ctx context.Context) (*firestore.Client, error) {
+func Firebase(ctx context.Context) (*firebase.App, error) {
 	saJSON, err := base64.StdEncoding.DecodeString(os.Getenv("FIRESTORE_SA"))
 	if err != nil {
 		return nil, err
 	}
 	sa := option.WithCredentialsJSON(saJSON)
-	app, err := firebase.NewApp(ctx, nil, sa)
-	if err != nil {
-		return nil, err
-	}
 
-	client, err := app.Firestore(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
+	return firebase.NewApp(ctx, nil, sa)
 }
---
+++ internal/update/pubsub.go
@@ -0,0 +1,40 @@
+package update
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/pubsub"
+)
+
+// getOrCreateTopic gets a topic or creates it if it doesn't exist.
+func getOrCreateTopic(ctx context.Context, client *pubsub.Client, topicID string) (*pubsub.Topic, error) {
+	topic := client.Topic(topicID)
+	ok, err := topic.Exists(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check if topic exists: %v", err)
+	}
+	if !ok {
+		topic, err = client.CreateTopic(ctx, topicID)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create topic (%q): %v", topicID, err)
+		}
+	}
+	return topic, nil
+}
+
+// getOrCreateSub gets a subscription or creates it if it doesn't exist.
+func getOrCreateSub(ctx context.Context, client *pubsub.Client, subID string, cfg *pubsub.SubscriptionConfig) (*pubsub.Subscription, error) {
+	sub := client.Subscription(subID)
+	ok, err := sub.Exists(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check if subscription exists: %v", err)
+	}
+	if !ok {
+		sub, err = client.CreateSubscription(ctx, subID, *cfg)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create subscription (%q): %v", topicID, err)
+		}
+	}
+	return sub, nil
+}

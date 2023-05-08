d45f8acf0d07ff5b78bbced8ecee1c7ebe5b14d9
Amarjeet Singh Rai <git@arai.dev>
2023-05-08 18:36:40 +0100

    rename middleware

--- internal/middleware/jsonContentType.go
+++ internal/middleware/jsonContentType.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 
-func JSONHeaders(next http.Handler) http.Handler {
+func JSONContentType(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
